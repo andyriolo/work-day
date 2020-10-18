a9387ab117822546f5cd0d3df265535868c0f745
Andrew Riolo <andyriolo17@gmail.com>
2020-10-18 16:37:15 -0700

    attempted to add the color changing aspect

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,56 @@
+
+
 //Time Display
 var a = moment().format('LLL');
 console.log(a);
 
 var a = $(".timer").text(a);
+
+
+Change textarea background color based on time
+var checkTime = function () {
+
+    //Get Current time
+    var currentTime = moment().format('H');
+    console.log(currentTime);
+    var currentTime = function() {
+        if (currentTime===9){
+            document.getElementsByClassName("9a")
+            console.log("9a");
+            elem.classList.add("present");
+        } else if { (currentTime===10)
+            document.getElementsByClassName("10a")
+            console.log("10a");
+            elem.classList.add("present");
+        } else if { (currentTime===11)
+            document.getElementsByClassName("11a")
+            console.log("11a");
+            elem.classList.add("present");
+        } else if { (currentTime===12)
+            document.getElementsByClassName("12p")
+            console.log("12p");
+            elem.classList.add("present");
+        } else if { (currentTime===13)
+            document.getElementsByClassName("1p")
+            console.log("1p");
+            elem.classList.add("present");
+        } else if { (currentTime===14)
+            document.getElementsByClassName("2p")
+            console.log("2p");
+            elem.classList.add("present");
+        } else if {(currentTime===15)
+            document.getElementsByClassName("3p")
+            console.log("3p");
+            elem.classList.add("present");
+        } else if {(currentTime===16)
+            document.getElementsByClassName("4p")
+            console.log("4p");
+            elem.classList.add("present");
+        } else { (currentTime===17)
+            document.getElementsByClassName("5p")
+            console.log("5p");
+            elem.classList.add("present");
+        }
+}
+
+I DONT KNOW
